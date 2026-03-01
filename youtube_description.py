8c7375c94189b5cf730f51237a72b69957863faa
basha1968 <anseranwarbasha754@gmail.com>
2026-03-01 20:27:34 +0530

    Update youtube_description.py

--- youtube_description.py
+++ youtube_description.py
@@ -1,1 +1,9 @@
 print("Generating YouTube description...")
+print("""
+Warehouse Safety Training
+
+In this video we discuss key warehouse safety rules based on real industrial experience.
+
+Read full guide:
+https://safetybeyondrules.blogspot.com
+""")
