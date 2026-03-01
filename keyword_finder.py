2dda3284a27ec7c7a57aca3507efeb9f9d7cfc77
basha1968 <anseranwarbasha754@gmail.com>
2026-03-01 19:49:41 +0530

    Add keyword finder functionality

---
+++ keyword_finder.py
@@ -0,0 +1,12 @@
+keywords = [
+"construction safety tips",
+"warehouse safety checklist",
+"PPE safety rules",
+"industrial accident prevention"
+]
+
+import random
+
+keyword = random.choice(keywords)
+
+print("Selected keyword:", keyword)
